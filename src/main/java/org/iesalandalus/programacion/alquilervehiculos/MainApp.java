33bc44f26c2b98c6b680b9a36ff74e3a64a43b8e
Juanma <juanmi_4000@hotmail.es>
2023-04-25 10:56:57 +0200

    He cambiado el readme

---
+++ src/main/java/org/iesalandalus/programacion/alquilervehiculos/MainApp.java
@@ -0,0 +1,24 @@
+package org.iesalandalus.programacion.alquilervehiculos;
+
+import javax.naming.OperationNotSupportedException;
+
+import org.iesalandalus.programacion.alquilervehiculos.controlador.Controlador;
+import org.iesalandalus.programacion.alquilervehiculos.modelo.FactoriaFuenteDatos;
+import org.iesalandalus.programacion.alquilervehiculos.modelo.Modelo;
+import org.iesalandalus.programacion.alquilervehiculos.modelo.ModeloCascada;
+import org.iesalandalus.programacion.alquilervehiculos.vista.FactoriaVista;
+import org.iesalandalus.programacion.alquilervehiculos.vista.Vista;
+
+public class MainApp {
+
+	public static void main(String[] args) throws OperationNotSupportedException {
+		Modelo modelo = new ModeloCascada(FactoriaFuenteDatos.FICHEROS);
+		Vista vista = FactoriaVista.TEXTO.crear();
+
+		Controlador controlador = new Controlador(modelo, vista);
+
+		controlador.comenzar();
+
+	}
+
+}
