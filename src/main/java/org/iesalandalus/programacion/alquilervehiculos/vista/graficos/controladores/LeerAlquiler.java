ac780d91ef84fbd6ba3b78f7b055c3d2897de491
juanm <juanm@DESKTOP-TALILS6>
2023-05-11 19:49:42 +0200

    Seguridad

---
+++ src/main/java/org/iesalandalus/programacion/alquilervehiculos/vista/graficos/controladores/LeerAlquiler.java
@@ -0,0 +1,38 @@
+package org.iesalandalus.programacion.alquilervehiculos.vista.graficos.controladores;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
+public class LeerAlquiler {
+
+    @FXML
+    private Button btCancelar;
+
+    @FXML
+    private DatePicker dpFechaAlquiler;
+
+    @FXML
+    private DatePicker dpFechaDevolucion;
+
+    @FXML
+    private TextField tfCliente;
+
+    @FXML
+    private TextField tfVehiculo;
+
+    @FXML
+    void cancelar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void insertarAlquiler(ActionEvent event) {
+
+    }
+
+}
+
+
